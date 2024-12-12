87c39ecd0a231734e77acb4aee1e3466660ab563
Akshay Pariyar <akshaypariyar@Akshays-MacBook-Air.local>
2024-12-13 01:04:56 +0530

    fixing backend

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,41 @@
+// Set NODE_ENV to production if not already set
+process.env.NODE_ENV = process.env.NODE_ENV || "production";
+
+// Load environment variables from .env file
+require('dotenv').config();
+
 const express = require("express");
-const{ApolloServer} = require('apollo-server-express');
-const connectDB = require('./config/db')
-const typeDefs = require('./graphql/typeDefs')
-const resolver = require('./graphql/resolvers')
+const { ApolloServer } = require('apollo-server-express');
+const connectDB = require('./config/db');
+const typeDefs = require('./graphql/typeDefs');
+const resolver = require('./graphql/resolvers');
 const cors = require('cors');
-//MiddleWare 
+
+// Middleware 
 const app = express();
 app.use(cors());
 
-//connecting to data base
+// Connect to the database
 connectDB();
 app.use(express.json());
 
-app.use('/api/users',require('./routes/api/users'));
-app.use('/api/users',require('./routes/api/auth'));
-app.use('/api/users',require('./routes/api/update'));
-app.use('/api/users',require('./routes/api/profile'));
-app.use('/api/users',require('./routes/api/account'))
+// Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/users', require('./routes/api/auth'));
+app.use('/api/users', require('./routes/api/update'));
+app.use('/api/users', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/account'));
 
-async function startServer(){
-      // const server = new ApolloServer({typeDefs,resolver});
-      // await server.start(); // Ensure the server starts before applying middleware
-      // server.applyMiddleware({app,path:'/gql'});
-      const PORT  = 8000;
-      app.listen(PORT,()=>{
-         console.log(`server starts on port ${PORT}`)
-      //   console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`)
-      } )
+// Start Apollo Server and Express App
+async function startServer() {
+    // const server = new ApolloServer({ typeDefs, resolver });
+    // await server.start(); // Ensure the server starts before applying middleware
+    // server.applyMiddleware({ app, path: '/gql' });
+
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => {
+        console.log(`Server starts on port ${PORT}`);
+        // console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+    });
 }
 startServer();
-
-
-
-
